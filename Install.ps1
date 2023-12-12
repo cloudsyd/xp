bfe0427aafceb7e1759657ec83115b97c4d9b87f
cloudsyd <153617471+cloudsyd@users.noreply.github.com>
2023-12-12 13:36:25 +0100

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,53 +1,82 @@
-$tmp = "C:\tmp\"
+param (
+[string]
+$Username,
 
-# Pattern match the installer name since it changes with every version.
-$in = Get-ChildItem $tmp -Recurse -Include "Milestone XProtect VMS Products *.exe"
+[string]
+$Password
+)
 
-# Pattern match the license file name since it could be change with every version.
-$license = Get-ChildItem $tmp -Recurse -Include "*.lic"
+$block = {
+$tmp = "C:\tmp\";
+
+$installer = Get-ChildItem $tmp `
+-Recurse `
+-Include "Milestone XProtect VMS Products *.exe";
+$license = Get-ChildItem $tmp `
+-Recurse `
+-Include "*.lic";
 
 # Generate arguments file for the installer in order to use custom user parameters
-$aP = Start-Process $in.FullName -ArgumentList "--generateargsfile=$tmp" -PassThru
-Wait-Process $aP.Id
+$argumentProcess = Start-Process $installer.FullName `
+-ArgumentList "--generateargsfile=$tmp" `
+-PassThru `
+-Verb runAs
+Wait-Process $argumentProcess.Id
 
 # Read the newly created arguments file
 $argumentsXml = [System.Xml.XmlDocument](Get-Content "$($tmp)Arguments.xml")
 
 # Define the arguments and their values as a hashtable
 $newArguments = @{
-    "SQL-KEEP-DATA" = "no"
+"SQL-KEEP-DATA" = "no"
 }
 
-foreach ($keys in $newArguments.Keys) {
-$ar = $argumentsXml.ArgumentsXmlV2.Arguments.Argument | Where-Object { $_.Name -eq $keys }
+foreach ($newArgument in $newArguments.Keys) {
+$elementArgument = $argumentsXml.ArgumentsXmlV2.Arguments.Argument | Where-Object { $_.Name -eq $newArgument }
 
-if ($ar) {
-    # Update the value of the existing argument
-    $ar.Value = $newArguments[$keys]
-}
-else {
+if ($elementArgument) {
+# Update the value of the existing argument
+$elementArgument.Value = $newArguments[$newArgument]
+} 
+else
+{
 # Create a new argument object
-$ar = $argumentsXml.CreateElement("Argument", $argumentsXml.DocumentElement.NamespaceURI)
+$newElement = $argumentsXml.CreateElement("Argument", $argumentsXml.DocumentElement.NamespaceURI)
 
 # Create Name, Description, and Value elements and set their inner text
-$name = $argumentsXml.CreateElement("Name", $argumentsXml.DocumentElement.NamespaceURI)
-$name.InnerText = $keys
+$nameElement = $argumentsXml.CreateElement("Name", $argumentsXml.DocumentElement.NamespaceURI)
+$nameElement.InnerText = $newArgument
 
-$description = $argumentsXml.CreateElement("Description", $argumentsXml.DocumentElement.NamespaceURI)
-$description.InnerText = "" # You can add a description for each argument in the hashtable if needed
+$descriptionElement = $argumentsXml.CreateElement("Description", $argumentsXml.DocumentElement.NamespaceURI)
+$descriptionElement.InnerText = ""
 
-$value = $argumentsXml.CreateElement("Value", $argumentsXml.DocumentElement.NamespaceURI)
-$value.InnerText = $newArguments[$keys]
+$valueElement = $argumentsXml.CreateElement("Value", $argumentsXml.DocumentElement.NamespaceURI)
+$valueElement.InnerText = $newArguments[$newArgument]
 
 # Append Name, Description, and Value elements as children to the new Argument element
-$ar.AppendChild($name)
-$ar.AppendChild($description)
-$ar.AppendChild($value)
+$newElement.AppendChild($nameElement)
+$newElement.AppendChild($descriptionElement)
+$newElement.AppendChild($valueElement)
 
 # Add the new argument to the Arguments collection
-$argumentsXml.ArgumentsXmlV2.Arguments.AppendChild($ar)
-  }
+$argumentsXml.ArgumentsXmlV2.Arguments.AppendChild($newElement)
+}
 }
 
 # Save arguments file with modified user data
 $argumentsXml.Save("$($tmp)Arguments.xml");
+}
+$block > C:\tmp\setup.ps1
+
+# Convert the password to a secure string
+$securePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+
+# Create a credential object
+$c = New-Object System.Management.Automation.PSCredential($Username, $securePassword)
+
+# Start the PowerShell process with the specified script and credentials
+$sP = Start-Process PowerShell -ArgumentList "-File C:\tmp\setup.ps1" -Credential $c -Wait
+
+Wait-Process $sP.Id;
+
+Write-Output "Generated the argument file"
